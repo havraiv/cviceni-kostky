3a601c66231ee45f9f15b09c6cd8fd733566d00c
Iva Havranová <havranova.i@seznam.cz>
2023-04-20 20:48:13 +0200

    img přesunuty

--- Dice/index.js
+++ Dice/index.js
@@ -1,6 +1,7 @@
 export const Dice = (props) => {
   const { side } = props;
   const element = document.createElement('div');
+  element.classList.add('dice');
 
   element.innerHTML = `<div class="dice__side dice__side--${side}"></div>
   <button class="btn btn--small roll-btn">hodit</button>`;
